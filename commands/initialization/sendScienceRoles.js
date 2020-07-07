986aa196cc0c6c5de0b08874508b845bd74fd520
Jacob Drury <jacobdrury75@gmail.com>
2020-07-06 23:11:49 -0500

    add science roles message

---
+++ commands/initialization/sendScienceRoles.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const ReactionMessage = require('../../db/models/reactionMessages.js');
+const { MessageEmbed } = require('discord.js');
+const { getRoles } = require('../../modules/utils.js');
+module.exports = {
+    name: 'sendMathRoles',
+    description: `Sends an embed for the reaction roles for the science roles`,
+    usage: `use in channel you want the message sent in`,
+    guildOnly: true,
+    adminOnly: true,
+    category: 'initialization',
+    async execute(message) {
+        const embedMessage = await message.channel.send({
+            embed: new MessageEmbed({
+                color: 0x1e90ff,
+                title: 'Science Roles',
+                description:
+                    '1️) Computer Science\n' +
+                    '2️) Biology\n' +
+                    '3️) Physics\n' +
+                    '4️) Physical Science\n' +
+                    '5️) Chemistry',
+            }),
+        });
+
+        const roles = await getRoles(message.client);
+
+        if (!roles) return message.reply("Cannot find 'Roles' in the database");
+
+        const emojis = new Map();
+        emojis.set('1️⃣', roles.compSci.Id);
+        emojis.set('2️⃣', roles.biology.Id);
+        emojis.set('3️⃣', roles.physics.Id);
+        emojis.set('4️⃣', roles.physScience.Id);
+        emojis.set('5️⃣', roles.chemistry.Id);
+
+        emojis.forEach(async (value, emoji) => await embedMessage.react(emoji));
+
+        await message.delete();
+
+        const reactionMessage = new ReactionMessage({
+            _id: mongoose.Types.ObjectId(),
+            messageId: embedMessage.id,
+            reactions: {},
+        });
+
+        reactionMessage.reactions = emojis;
+
+        return reactionMessage.save();
+    },
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Client, Collection } = require('discord.js');
-require('events').EventEmitter.defaultMaxListeners = 15;
+require('events').EventEmitter.defaultMaxListeners = 20;
 
 const client = new Client();
 ['commands', 'aliases'].forEach((x) => (client[x] = new Collection()));
 
-['event', 'command'].forEach((handler) => require(`./handlers/${handler}`)(client));
-
+['event', 'command'].forEach((handler) =>
+    require(`./handlers/${handler}`)(client)
+);
 client.prefix = process.env.PREFIX;
 client.guildId = process.env.GUILD_ID;
 client.mongoose = require('./db/mongoose');
