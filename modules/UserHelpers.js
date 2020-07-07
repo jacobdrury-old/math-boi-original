514452dc7271b8243cba2f1814655661c843be65
Jacob Drury <jacobdrury75@gmail.com>
2020-07-06 23:06:27 -0500

    add remove role function

--- modules/UserHelpers.js
+++ modules/UserHelpers.js
@@ -35,3 +35,39 @@ exports.setToRole = async (member, role, adminId = null, shouldLog = true) => {
         }
     }
 };
+
+exports.removeRole = async (member, role, adminId = null, shouldLog = true) => {
+    await member.roles.remove(role);
+
+    if (shouldLog) {
+        await member.send({
+            embed: await removeRoleEmbedForUser(member, role),
+        });
+
+        const logChannel = await getLogChannel(member.client);
+        if (logChannel) {
+            const webhookClient = new WebhookClient(
+                logChannel.Id,
+                logChannel.token
+            );
+            const embed = logEmbed(member, 'Role Removed').addFields(
+                {
+                    name: 'User',
+                    value: `<@!${member.id}>`,
+                    inline: true,
+                },
+                {
+                    name: 'Role',
+                    value: `<@&${role}>`,
+                    inline: true,
+                }
+            );
+
+            if (adminId) {
+                embed.addField('Added By', `<@!${adminId}>`);
+            }
+
+            await webhookClient.send({ embeds: [embed] });
+        }
+    }
+};
