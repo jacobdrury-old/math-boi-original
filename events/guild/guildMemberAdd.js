ed430924a92e095a2f1e2cd4b26eec6ef8045025
Jacob Drury <jacobdrury75@gmail.com>
2020-07-08 16:38:32 -0500

    remove default role

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -4,11 +4,14 @@ const { logEmbed, newUserEmbed } = require('../../modules/embeds.js');
 const { setToRole } = require('../../modules/UserHelpers.js');
 const { getLogChannel } = require('../../modules/utils.js');
 module.exports = async (client, member) => {
+    return;
     // #region Add unverified role to new user
     const roles = await getRoles(client);
 
     if (!roles.unverified) {
-        return await member.send('Could not find the unverified role. If this continues please contact an Admin');
+        return await member.send(
+            'Could not find the unverified role. If this continues please contact an Admin'
+        );
     }
 
     setToRole(member, roles.unverified.Id, null, false);
@@ -18,8 +21,13 @@ module.exports = async (client, member) => {
 
     const logChannel = await getLogChannel(member.client);
     if (logChannel) {
-        const webhookClient = new WebhookClient(logChannel.Id, logChannel.token);
-        const embed = logEmbed(member, 'User Joined').setColor('#00d166').addField('User', `<@!${member.id}>`, true);
+        const webhookClient = new WebhookClient(
+            logChannel.Id,
+            logChannel.token
+        );
+        const embed = logEmbed(member, 'User Joined')
+            .setColor('#00d166')
+            .addField('User', `<@!${member.id}>`, true);
 
         webhookClient.send({ embeds: [embed] });
     }
