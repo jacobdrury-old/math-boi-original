d8cb31aebe5dbd9a3506bffe68deee911880a795
Jacob Drury <jacobdrury75@gmail.com>
2020-07-23 11:19:11 -0500

    update default cool down

--- events/guild/message.js
+++ events/guild/message.js
@@ -73,7 +73,7 @@ module.exports = async (client, message) => {
 
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
+    const cooldownAmount = (command.cooldown || 0) * 1000;
 
     if (timestamps.has(message.author.id)) {
         const expirationTime =
