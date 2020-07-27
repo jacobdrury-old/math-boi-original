555df6aad348e841d470c195d9fa663f8603593b
Jacob Drury <jacobdrury75@gmail.com>
2020-07-26 23:02:26 -0500

    remove commented code

--- modules/mee6.js
+++ modules/mee6.js
@@ -13,48 +13,6 @@ exports.readLogs = async (client, message) => {
     const member = await message.guild.members.fetch(memberId);
 
     return member.send('', { embed: getEmbed(member, punishment, embed) });
-    /*
-    if (punishment === 'WARN') {
-        const reason = embed.fields[2].value;
-        return user.send('', {
-            embed: {
-                color: 0xffa500,
-                author: {
-                    name: `[WARNING] ${user.user.username}#${user.user.discriminator}.`,
-                    icon_url: user.user.displayAvatarURL(),
-                },
-                description: `You have received a warning.`,
-                fields: [{ name: 'Reason', value: reason }],
-            },
-        });
-    } else if (punishment === 'MUTE') {
-        const reason = embed.fields[2].value;
-        const duration = embed.fields[3].value;
-        return user.send('', {
-            embed: {
-                color: 0xff0000,
-                author: {
-                    name: `[MUTED] ${user.user.username}#${user.user.discriminator}`,
-                    icon_url: user.user.displayAvatarURL(),
-                },
-                description: `You have been muted.`,
-                fields: [
-                    { name: 'Reason', value: reason },
-                    { name: 'Duration', value: duration },
-                ],
-            },
-        });
-    } else if (punishment === 'UNMUTE') {
-        return user.send('', {
-            embed: {
-                color: 0x008000,
-                author: {
-                    name: `[UNMUTE] You have been unmuted!`,
-                    icon_url: user.user.displayAvatarURL(),
-                },
-            },
-        });
-    }*/
 };
 
 const getEmbed = (member, punishment, embed) => {
