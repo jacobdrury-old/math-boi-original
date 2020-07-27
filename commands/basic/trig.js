ae5317dda9378084d53a528ee1eaa015879c2614
Jacob Drury <jacobdrury75@gmail.com>
2020-07-27 01:06:44 -0500

    add trig command

---
+++ commands/basic/trig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'trig',
+    description: 'Sends the trig identities',
+    category: 'basic',
+    execute(message) {
+        message.channel.send(
+            'https://media.discordapp.net/attachments/725174934665429024/731589189748916234/image0.png?width=208&height=270'
+        );
+    },
+};
