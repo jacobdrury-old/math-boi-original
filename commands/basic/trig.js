dbb3c911e4ed704188c952d6eecf28ccebbce067
Jacob Drury <jacobdrury75@gmail.com>
2020-07-27 01:15:44 -0500

    sdf

--- commands/basic/trig.js
+++ commands/basic/trig.js
@@ -4,7 +4,7 @@ module.exports = {
     category: 'basic',
     execute(message) {
         message.channel.send(
-            'https://media.discordapp.net/attachments/725174934665429024/731589189748916234/image0.png?width=208&height=270'
+            'https://cdn.discordapp.com/attachments/725174934665429024/731589189748916234/image0.png'
         );
     },
 };
