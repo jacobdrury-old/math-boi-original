24fc697a8839d652e655e55e837afd0799f9c4fa
Jacob Drury <jacob@Jacobs-MacBook-Pro.local>
2020-08-25 12:50:36 -0500

    add args to invite cmd

--- commands/basic/invite.js
+++ commands/basic/invite.js
@@ -2,8 +2,25 @@ module.exports = {
     name: 'invite',
     description: 'Provides a crisp invite to the server',
     category: 'basic',
-    async execute(message) {
-        await message.author.send('https://discord.gg/S2azCgw');
-        await message.reply("Check your DM's for the invite link!");
+    guildOnly: true,
+    async execute(message, args) {
+        let member = message.author;
+        const mention = args[0];
+        if (mention && mention.startsWith('<@') && mention.endsWith('>')) {
+            const id = mention
+                .replace('<@', '')
+                .replace('!', '')
+                .replace('>', '');
+            if (!isNaN(id) && id.length == 18) {
+                member = await message.client.guilds.cache
+                    .get(client.guildId)
+                    .members.fetch(id);
+            }
+
+            await member.send('https://discord.gg/S2azCgw');
+            await message.channel.send(
+                `${member} Check your DM's for the invite link!`
+            );
+        }
     },
 };
