00d480fb42f8e65babf600a0aa4f29ed8169e748
Jacob Drury <jacob@Jacobs-MacBook-Pro.local>
2020-08-26 22:58:16 -0500

    add meme cmds

---
+++ commands/meme/lexi.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'lexi',
+    description: 'lexi',
+    category: 'meme',
+    execute(message) {
+        message.channel.send('I dont wan to get married');
+    },
+};
