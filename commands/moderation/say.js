3674873152060dd15440e4b5ecd32714603e695d
Jacob Drury <jacob@Jacobs-MacBook-Pro.local>
2020-08-28 01:04:57 -0500

    I've been defeated

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -6,8 +6,6 @@ module.exports = {
     guildOnly: true,
     async execute(message, args) {
         await message.delete();
-        const fakeMsg = await message.channel.send('pls no snipe');
-        fakeMsg.delete();
         await message.channel.send(args.join(' '));
     },
 };
