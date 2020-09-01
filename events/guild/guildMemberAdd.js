06efa477b8294940fe7c670bd240fe2cf560160f
Jacob Drury <jacob@Jacobs-MacBook-Pro.local>
2020-08-31 23:00:46 -0500

    add auto moderation to nickname

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,8 +1,24 @@
-const { getRoles } = require('../../modules/utils.js');
 const { WebhookClient } = require('discord.js');
-const { logEmbed, newUserEmbed } = require('../../modules/embeds.js');
-const { setToRole } = require('../../modules/UserHelpers.js');
+const { logEmbed } = require('../../modules/embeds.js');
 const { getLogChannel } = require('../../modules/utils.js');
 module.exports = async (client, member) => {
-    
+    const nickname = member.displayName;
+    const regex = /^[a-zA-Z0-9?{}[\]_!@#$\^&*|']*$/;
+
+    if (nickname.match(regex)) {
+        await member.setNickname('Moderated Nickname', 'Invalid username');
+
+        const logChannel = await getLogChannel(member.client);
+        if (logChannel) {
+            const webhookClient = new WebhookClient(
+                logChannel.Id,
+                logChannel.token
+            );
+            const embed = logEmbed(member, 'Invalid Username')
+                .setDescription(`${member}'s nickname has been auto moderated`)
+                .addField('Invalid Name', nickname);
+
+            await webhookClient.send({ embeds: [embed] });
+        }
+    }
 };
