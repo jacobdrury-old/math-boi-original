bc7ce0893a3317013af86e9799493cc191326fa7
Jacob Drury <jacob@Jacobs-MacBook-Pro.local>
2020-09-04 23:56:17 -0500

    add auto send verify msg

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,6 @@
+const cron = require('cron');
+const clean = require('../../commands/admin/clean.js').execute
+const welcomeId = '737118741510357063';
 module.exports = (client) => {
     // client.user.setPresence({
     //     status: 'online',
@@ -6,5 +9,15 @@ module.exports = (client) => {
     //         type: 'WATCHING',
     //     },
     // });
+
     console.log('Ready!');
+
+    //Auto send verify message everyday at noon
+    cron.schedule("0 12 * * *", () => {
+        const channel = client.guilds.cache.get(client.guildId).first().channels.cache.get(welcomeId);
+        await clean()
+        await channel.send(
+            'Hey! @everyone I see you guys are not verified yet, please go check out #📜rules Then all you have to do it react the the :white_check_mark: to have access to the server! If you have any issues please tag @Staff 🧙‍♂️ . :slight_smile: Once you Verify you should check out #💎role-selection Thank you!'
+        );
+    });
 };
