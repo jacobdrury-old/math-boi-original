48295ce30aeb66211f6e306aba5b88282d0402a3
Jacob Drury <jacobdrury75@gmail.com>
2020-09-05 12:14:27 -0500

    add logging to cron job

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 const CronJob = require('cron').CronJob;
-const clean = require('../../commands/admin/clean.js').execute;
+const { getLogChannel } = require('../../modules/utils.js');
 const helpId = '737118741510357063';
 module.exports = (client) => {
     // client.user.setPresence({
@@ -13,16 +13,28 @@ module.exports = (client) => {
     console.log('Ready!');
 
     //Auto send verify message everyday at noon
-    const job = new CronJob('0 12 * * *', async () => {
-        const channel = client.guilds.cache
-            .get(client.guildId)
-            .first()
-            .channels.cache.get(helpId);
+    try {
+        const job = new CronJob('0 12 * * *', async () => {
+            const channel = client.guilds.cache
+                .get(client.guildId)
+                .first()
+                .channels.cache.get(helpId);
 
-        await channel.bulkDelete(20, true);
-        await channel.send(
-            'Hey! @everyone I see you guys are not verified yet, please go check out #📜rules Then all you have to do it react the the :white_check_mark: to have access to the server! If you have any issues please tag @Staff 🧙‍♂️ . :slight_smile: Once you Verify you should check out #💎role-selection Thank you!'
-        );
-    });
-    job.start();
+            await channel.bulkDelete(20, true);
+            await channel.send(
+                'Hey! @everyone I see you guys are not verified yet, please go check out #📜rules Then all you have to do it react the the :white_check_mark: to have access to the server! If you have any issues please tag @Staff 🧙‍♂️ . :slight_smile: Once you Verify you should check out #💎role-selection Thank you!'
+            );
+        });
+        job.start();
+    } catch (err) {
+        const logChannel = await getLogChannel(client);
+        if (logChannel) {
+            const webhookClient = new WebhookClient(
+                logChannel.Id,
+                logChannel.token
+            );
+            await webhookClient.send(`${err}`);
+        }
+        console.error(err)
+    }
 };
