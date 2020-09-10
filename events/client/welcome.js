dc63782bdf061df582919dc792758e11f2d4e0eb
Jacob Drury <jacobdrury75@gmail.com>
2020-09-10 18:50:43 -0500

    add ignored channels to msg count

--- events/client/welcome.js
+++ events/client/welcome.js
@@ -1,5 +1,16 @@
 const generalId = '729870525119332414';
 
+const IGNORED = new Set([
+    '737457798123880509',
+    '730264204174688288',
+    '725171177235939378',
+    '725178491494072381',
+    '725172886821666898',
+    '740086491069546537',
+    '751999833597804624',
+    '752000303787933706',
+]);
+
 module.exports = async (client, member) => {
     const generalChat = member.guild.channels.cache.get(generalId);
 
@@ -10,7 +21,9 @@ module.exports = async (client, member) => {
 };
 
 const userMessages = async (guild, userID) => {
-    const txtChannels = guild.channels.cache.filter((c) => c.type === 'text');
+    const txtChannels = guild.channels.cache.filter(
+        (c) => c.type === 'text' && !IGNORED.has(c.parentID)
+    );
 
     const msgCount = txtChannels.reduce(async (totalP, ch) => {
         const total = await totalP;
