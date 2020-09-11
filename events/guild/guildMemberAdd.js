c1bc3a2a441aed19bd08f627eb455961bd140dc8
Jacob Drury <jacobdrury75@gmail.com>
2020-09-11 11:26:07 -0500

    disable auto nickname auto mod

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,23 +2,20 @@ const { WebhookClient } = require('discord.js');
 const { logEmbed } = require('../../modules/embeds.js');
 const { getLogChannel } = require('../../modules/utils.js');
 module.exports = async (client, member) => {
-    const nickname = member.displayName;
-    const regex = /^[a-zA-Z0-9?{}\.[\]_\-!@#$\^&*|']*$/;
-
-    if (!nickname.replace(' ', '').match(regex)) {
-        await member.setNickname('Moderated Nickname', 'Invalid username');
-
-        const logChannel = await getLogChannel(member.client);
-        if (logChannel) {
-            const webhookClient = new WebhookClient(
-                logChannel.Id,
-                logChannel.token
-            );
-            const embed = logEmbed(member, 'Invalid Username')
-                .setDescription(`${member}'s nickname has been auto moderated`)
-                .addField('Invalid Name', nickname);
-
-            await webhookClient.send({ embeds: [embed] });
-        }
-    }
+    // const nickname = member.displayName;
+    // const regex = /^[a-zA-Z0-9?{}\.[\]_\-!@#$\^&*|']*$/;
+    // if (!nickname.replace(' ', '').match(regex)) {
+    //     await member.setNickname('Moderated Nickname', 'Invalid username');
+    //     const logChannel = await getLogChannel(member.client);
+    //     if (logChannel) {
+    //         const webhookClient = new WebhookClient(
+    //             logChannel.Id,
+    //             logChannel.token
+    //         );
+    //         const embed = logEmbed(member, 'Invalid Username')
+    //             .setDescription(`${member}'s nickname has been auto moderated`)
+    //             .addField('Invalid Name', nickname);
+    //         await webhookClient.send({ embeds: [embed] });
+    //     }
+    // }
 };
