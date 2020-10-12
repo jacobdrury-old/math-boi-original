0c55203c15e3866b7a6e06e65a841320b89378c3
Jacob Drury <jacobdrury75@gmail.com>
2020-10-12 11:58:15 -0500

    add pfp cmd

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -4,20 +4,8 @@ module.exports = {
     guildOnly: true,
     category: 'admin',
     async execute(message) {
-        const guild = message.member.guild;
-        await message.channel.send('', {
-            embed: {
-                color: 0x42648a,
-                title: `**Welcome to ${guild.name}!**`,
-                thumbnail: {
-                    url: guild.iconURL({ dynamic: true }),
-                },
-                description:
-                    `**In order to get help you must first:**\n` +
-                    `- Go and read the rules and react with a :white_check_mark:\n\n` +
-                    `- Go to the role selection channel and add the subjects you need help with!\n\n` +
-                    `- Post your question in the corresponding channel and tag \`@Tutor\``,
-            },
-        });
+        await message.channel.send(
+            message.guild.me.user.avatarURL({ dynamic: true })
+        );
     },
 };
