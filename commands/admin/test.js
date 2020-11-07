0994833bb6dcf2fa0b3a81f1b790037cdb4821cb
Jacob Drury <jacobdrury75@gmail.com>
2020-11-07 02:17:19 -0600

    clear test cmd

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -1,26 +1,7 @@
-const ReactionMessage = require('../../db/models/reactionMessages');
-const { getBlockedTutors } = require('../../modules/utils');
-
 module.exports = {
     name: 'test',
     adminOnly: true,
     guildOnly: true,
     category: 'admin',
-    async execute(message) {
-        // const blockedTutors = await getBlockedTutors();
-        // blockedTutors.blocked.push(message.author.id);
-        // await blockedTutors.save();
-        // const reactionMessage = new ReactionMessage({
-        //     _id: mongoose.Types.ObjectId(),
-        //     messageId: message.author.id + '1',
-        //     blockedUsers: blockedTutors,
-        // });
-        // await reactionMessage.save();
-        // const reactionMessage2 = await ReactionMessage.findOne({
-        //     messageId: message.author.id,
-        // }).populate('blockedUsers');
-        // console.log(reactionMessage2.blockedUsers.blocked);
-
-        console.log();
-    },
+    async execute(message) {},
 };
