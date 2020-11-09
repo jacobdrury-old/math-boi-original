ad1ae3cc8233fd2a828365257329d1d7dccbdbc7
Jacob Drury <jacobdrury75@gmail.com>
2020-11-08 22:52:47 -0600

    add try catch to bulk delete

--- events/logs/messageDeleteBulk.js
+++ events/logs/messageDeleteBulk.js
@@ -1,35 +1,37 @@
 const { getMessageLogChannel } = require('../../modules/utils');
 
 module.exports = async (client, messages) => {
-    if (!client.enableLogs) return;
-    if (client.ids.categories.admin === message.channel.parentID) return;
-    const webhookClient = await getMessageLogChannel();
-    if (!webhookClient) return;
+    try {
+        if (!client.enableLogs) return;
+        if (client.ids.categories.admin === message.channel.parentID) return;
+        const webhookClient = await getMessageLogChannel();
+        if (!webhookClient) return;
 
-    let embeds = [];
+        let embeds = [];
 
-    const send = async () => {
-        await webhookClient.send({ embeds });
-        embeds = [];
-    };
+        const send = async () => {
+            await webhookClient.send({ embeds });
+            embeds = [];
+        };
 
-    messages.forEach(async (message) => {
-        if (embeds.length == 10) {
-            await send();
-        }
+        messages.forEach(async (message) => {
+            if (embeds.length == 10) {
+                await send();
+            }
 
-        embeds.push({
-            author: {
-                name: `${message.member.user.username}#${message.member.user.discriminator}`,
-                icon_url: message.member.user.displayAvatarURL({
-                    dynamic: true,
-                }),
-            },
-            timestamp: new Date(),
-            color: 0xff2c02,
-            description: `**:wastebasket: Message sent by ${message.member} deleted in ${message.channel}**\n${message.content}`,
+            embeds.push({
+                author: {
+                    name: `${message.member.user.username}#${message.member.user.discriminator}`,
+                    icon_url: message.member.user.displayAvatarURL({
+                        dynamic: true,
+                    }),
+                },
+                timestamp: new Date(),
+                color: 0xff2c02,
+                description: `**:wastebasket: Message sent by ${message.member} deleted in ${message.channel}**\n${message.content}`,
+            });
         });
-    });
 
-    await send();
+        await send();
+    } catch (err) {}
 };
