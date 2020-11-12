6375afe57d399067000e0e2df57b0b5b16b7782c
Jacob Drury <jacobdrury75@gmail.com>
2020-11-12 13:43:15 -0600

    fix embed error

--- events/guild/bumpMessage.js
+++ events/guild/bumpMessage.js
@@ -1,10 +1,12 @@
 module.exports = async (client, message) => {
     setTimeout(() => {
-        embed
-            .setTitle("Bump timer's out!")
-            .setDescription("It's time to bump again!")
-            .setColor('#24b8b8')
-            .setTimestamp();
-        message.channel.send(`<@${client.ids.roles.bumping}>`, embed);
+        message.channel.send(`<@${client.ids.roles.bumping}>`, {
+            embed: {
+                title: "Bump timer's out!",
+                description: "It's time to bump again!",
+                color: 0x24b8b8,
+                timestamp: new Date(),
+            },
+        });
     }, 7200000);
 };
