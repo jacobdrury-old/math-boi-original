735344c07fec3bed19289f750ae52a3571d72777
Jacob Drury <jacobdrury75@gmail.com>
2020-11-30 01:26:46 -0600

    add initialization role for ping roles

---
+++ commands/roles/announcementPing.js
@@ -0,0 +1,26 @@
+const addRoleToDb = require('../../modules/RoleHelpers.js').addRoleToDb;
+const roleName = 'announcementPing';
+module.exports = {
+    name: roleName,
+    description: `Sets the ${roleName} role for the server`,
+    args: true,
+    usage: `<tag the ${roleName} role>`,
+    guildOnly: true,
+    ownerOnly: true,
+    category: 'roles',
+    execute(message, args) {
+        const rawId = args[0];
+        const roleId = rawId.replace('<@&', '').replace('>', '');
+        addRoleToDb(message.client, roleId, roleName, (err) => {
+            if (err) {
+                message.channel.send(
+                    `Could not set the ${roleName} role to ${rawId}`
+                );
+            } else {
+                message.channel.send(
+                    `Successfully set the ${roleName} role to ${rawId}`
+                );
+            }
+        });
+    },
+};
