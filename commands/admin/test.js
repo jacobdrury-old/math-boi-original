c074f079e5490fd9408555f203d8ad08c0bb63d0
Jacob Drury <jacobdrury75@gmail.com>
2020-11-29 23:08:44 -0600

    Update welcome message

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -3,19 +3,5 @@ module.exports = {
     adminOnly: true,
     guildOnly: true,
     category: 'admin',
-    async execute(message) {
-        // const verifiedRole = await message.guild.roles.fetch(
-        //     message.client.ids.roles.verified
-        // );
-        // const guildMembers = await message.guild.members.fetch();
-        // const unverifiedUsers = guildMembers.filter(
-        //     (member) =>
-        //         !member.roles.cache.get(message.client.ids.roles.verified) &&
-        //         !member.user.bot
-        // );
-        // for await ([id, member] of unverifiedUsers) {
-        //     await member.roles.set([]);
-        // }
-        // console.log('Done');
-    },
+    async execute(message) {},
 };
