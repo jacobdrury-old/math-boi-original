bc969b30deaa593ad2aeb6120f47b2ace4f58e45
Jacob Drury <35348468+jacobdrury75@users.noreply.github.com>
2021-01-15 00:27:58 -0600

    add angles cmd

---
+++ commands/basic/angles.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'angles',
+    category: 'basic',
+    execute(message) {
+        message.channel.send(
+            'https://cdn.discordapp.com/attachments/725174867867205703/766349857987493958/screen-shot-2019-02-07-at-4.png'
+        );
+    },
+};
