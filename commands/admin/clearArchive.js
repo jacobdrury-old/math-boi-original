5ed4840308a2fff55d1a944a96ff938239b3c898
Jacob Drury <jacobdrury75@gmail.com>
2021-02-17 16:43:19 -0600

    add clearArichive command

---
+++ commands/admin/clearArchive.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'clearArchive',
+    adminOnly: true,
+    guildOnly: true,
+    category: 'admin',
+    async execute(message) {
+        const client = message.client;
+
+        const guild = client.guilds.cache.get(client.guildId);
+
+        const archiveChannels = guild.channels.cache.filter(
+            (c) => c.parentID == client.ids.categories.archivedModMail
+        );
+
+        for (let [id, channel] of archiveChannels) {
+            await channel.delete();
+        }
+    },
+};
