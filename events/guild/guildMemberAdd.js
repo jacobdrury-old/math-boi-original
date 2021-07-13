427e11ae8b97e4674c4589491ea72b49521922cf
Jacob Drury <jacobdrury75@gmail.com>
2021-07-12 21:59:15 -0500

    update DM message

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -12,12 +12,10 @@ module.exports = async (client, member) => {
                 title: `**Welcome to ${guild.name}!**`,
                 description:
                     `**In order to get help you must first:**\n` +
-                    `- Go and read the rules and react with a :white_check_mark:\n\n` +
                     `- Go to the role selection channel and add the subjects you need help with!\n\n` +
                     `- Post your question in the corresponding channel and use the \`${client.prefix}tutor\` command`,
                 image: {
-                    url:
-                        'https://cdn.discordapp.com/attachments/731959466102226965/776323932890071061/banner_welcome1.png',
+                    url: 'https://cdn.discordapp.com/attachments/731959466102226965/776323932890071061/banner_welcome1.png',
                 },
             },
         });
